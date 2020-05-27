51ee080ff2404b2b83e5374f2c2fdb2ec44a3220
sebastianfuehr <s.fuehr1@gmail.com>
2020-05-27 17:45:34 +0200

    Add event class

---
+++ src/quarantine-client/objects/entities/event.ts
@@ -0,0 +1,24 @@
+import { PopupWindow } from "../../scenes/popupWindow";
+import { GuiScene } from "../../scenes/gui-scene";
+
+/**
+ * An in-game event and opens a popup window.  
+ * Represents the interface between the gui and the event
+ * application logic {@see EventController}.
+ * 
+ * @author Marvin Kruber
+ * @author Sebastian Führ
+ */
+export class Event {
+
+    /**
+     * @param executeEventFunction Should Encapsulates game logic which is executed when the event is triggered. E.g. increment the death counter (removal) of a citizen.
+     */
+    public constructor(executeEventFunction: Function, title: string, description: string, imagePath = "letter") {
+        const ppTitle = new Phaser.GameObjects.Text(GuiScene.instance, 550, 130, title, {});
+        //const popup = new PopupWindow(GuiScene.instance, 0, 0, 'log-background', 800, 600, true, [ppTitle], true);
+
+        executeEventFunction();
+    }
+
+}
